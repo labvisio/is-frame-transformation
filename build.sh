4b4a035c0912cf2b3e9494494a848a340046de0b
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-08-23 13:33:16 -0300

    feat: update conan and project (#3)
    * chore: update git ignore file
    
    * fix: build project with conan
    
    * fix: closes #1
    
    * fix: update shells scripts
    
    * fix: docker build
    
    * fix: update minimum cmake version
    
    * fix: fix typo in dockerfile
    
    * chore: update deployment manifest
    
    * chore: update example script
    
    * removed import from future

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-set -e 
+set -e
 
-mkdir -p build && cd build
-conan install .. -s compiler.libcxx=libstdc++11 --build=missing -o build_tests=True
-conan build ..
+conan build . \
+    --build=missing \
+    --output-folder=./ \
+    -s build_type=Release \
+    -s compiler.libcxx=libstdc++11 \
+    -o is-frame-transformation/*:build_tests=True
